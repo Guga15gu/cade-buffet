292429c726f34d83aa20baab7b9755a1f2ede78a
guga15gu <48689692+Guga15gu@users.noreply.github.com>
2024-04-17 15:36:22 -0300

    página inicial

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Usuario visita tela inicial' do
+  it 'e vê o nome da app' do
+    # Arrange
+
+    # Act
+    visit('/')
+    # Assert
+    expect(page).to have_content('Cade Buffet?')
+  end
+end
