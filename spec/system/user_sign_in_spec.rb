aae9105032daa4d091a951951baf82d08c3db087
guga15gu <48689692+Guga15gu@users.noreply.github.com>
2024-04-17 18:24:33 -0300

    adicionado teste usuário entra, e locales pt-BR

---
+++ spec/system/user_sign_in_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Usuário se autentica' do
+  it 'com sucesso' do
+    # Arrange
+    User.create!(email: 'gustavo@email.com', password: 'password', name: 'Gustavo')
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'gustavo@email.com'
+      fill_in 'Senha', with: 'password'
+
+      click_on 'Entrar'
+    end
+
+    # Assert
+    within('nav') do
+      expect(page).not_to have_link 'Entrar'
+      expect(page).to have_button 'Sair'
+      expect(page).to have_content 'gustavo@email.com'
+    end
+    expect(page).to have_content 'Login efetuado com sucesso.'
+  end
+end
