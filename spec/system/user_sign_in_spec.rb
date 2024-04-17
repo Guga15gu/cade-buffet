003392be774d81165c3a7c9879fd70b3f55260b4
guga15gu <48689692+Guga15gu@users.noreply.github.com>
2024-04-17 18:31:48 -0300

    usuário registra conta

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -23,4 +23,26 @@ describe 'Usuário se autentica' do
     end
     expect(page).to have_content 'Login efetuado com sucesso.'
   end
+
+  it 'e faz logout' do
+    # Arrange
+    User.create!(email: 'gustavo@email.com', password: 'password', name: 'Gustavo')
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'gustavo@email.com'
+      fill_in 'Senha', with: 'password'
+
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    # Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_content 'gustavo@email.com'
+  end
 end
