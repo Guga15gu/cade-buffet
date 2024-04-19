a9360d3aa37ed49861465cd93243b3c9334d1ef4
guga15gu <48689692+Guga15gu@users.noreply.github.com>
2024-04-19 17:52:10 -0300

    Usuário edita um buffet e mantém os campos obrigatórios

--- spec/system/buffet/user_edit_buffet_spec.rb
+++ spec/system/buffet/user_edit_buffet_spec.rb
@@ -52,4 +52,56 @@ describe 'Usuário edita um buffet' do
     expect(page).to have_content('Descrição: Buffet especializado em casamentos')
     expect(page).to have_content('Meios de Pagamento: Cheque, Cartão de crédito, Dinheiro')
   end
+
+  it 'e mantém os campos obrigatórios' do
+    # Arrange
+    buffet_owner_user = BuffetOwnerUser.create!(email: 'gustavo@email.com', password: 'password', name: 'Gustavo')
+
+    buffet = Buffet.create!(
+      business_name: 'Buffet Delícias',
+      corporate_name: 'Empresa de Buffet Ltda',
+      registration_number: '12345678901234',
+      contact_phone: '(11) 1234-5678',
+      address: 'Rua dos Sabores, 123',
+      district: 'Centro',
+      state: 'São Paulo',
+      city: 'São Paulo',
+      postal_code: '12345-678',
+      description: 'Buffet especializado em eventos corporativos',
+      payment_methods: 'Cartão de crédito, Dinheiro',
+      buffet_owner_user: buffet_owner_user
+    )
+    # Act
+    login_as(buffet_owner_user)
+    visit root_path
+    click_on 'Buffet Delícias'
+    click_on 'Editar Buffet'
+
+    fill_in 'Nome Fantasia', with: ''
+    fill_in 'Razão Social', with: ''
+    fill_in 'CNPJ', with: ''
+    fill_in 'Telefone para Contato', with: ''
+    fill_in 'Endereço', with: ''
+    fill_in 'Bairro', with: ''
+    fill_in 'Estado', with: ''
+    fill_in 'Cidade', with: ''
+    fill_in 'CEP', with: ''
+    fill_in 'Descrição', with: ''
+    fill_in 'Meios de Pagamento', with: ''
+    click_on 'Enviar'
+
+    # Assert
+    expect(page).to have_content 'Não foi possível atualizar o Buffet'
+    expect(page).to have_content "Nome Fantasia não pode ficar em branco"
+    expect(page).to have_content "Razão Social não pode ficar em branco"
+    expect(page).to have_content "CNPJ não pode ficar em branco"
+    expect(page).to have_content "Telefone para Contato não pode ficar em branco"
+    expect(page).to have_content "Endereço não pode ficar em branco"
+    expect(page).to have_content "Bairro não pode ficar em branco"
+    expect(page).to have_content "Estado não pode ficar em branco"
+    expect(page).to have_content "Cidade não pode ficar em branco"
+    expect(page).to have_content "CEP não pode ficar em branco"
+    expect(page).to have_content "Descrição não pode ficar em branco"
+    expect(page).to have_content "Meios de Pagamento não pode ficar em branco"
+  end
 end
